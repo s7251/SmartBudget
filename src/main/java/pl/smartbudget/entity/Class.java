3a3a06ab4495c4cafe4cf9826d600a9343afff48
Krystian K <kulkry@gmail.com>
2016-05-15 21:13:26 +0200

    first

---
+++ src/main/java/pl/smartbudget/entity/Class.java
@@ -0,0 +1,12 @@
+package pl.smartbudget.entity;
+
+public class Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("test");
+
+	}
+
+}
